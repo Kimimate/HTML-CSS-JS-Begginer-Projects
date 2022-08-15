0a7a97ba36aedd5f53e43fc4bcb60ffcc258fd26
Kimimate <103746985+Kimimate@users.noreply.github.com>
2022-08-15 22:16:45 +0200

    Update main.js

---
+++ JavaScript-Lerning/main.js
@@ -0,0 +1,6 @@
+let number = Number(prompt('Enter a number: '));
+let percent = Number(prompt('Enter a %: ')) / 100;
+let percentage = number * percent;
+let sum = number + percentage1512;
+alert(`${percent} of ${number} = ${percentage}`);
+alert(`${number} + ${percentage} = ${sum}`);
