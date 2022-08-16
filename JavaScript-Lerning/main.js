915f2e25dd4664d6840dc7739c003ceeea9fad3a
Kimimate <103746985+Kimimate@users.noreply.github.com>
2022-08-16 15:06:00 +0200

    Update main.js

--- JavaScript-Lerning/main.js
+++ JavaScript-Lerning/main.js
@@ -1,6 +1,44 @@
-let number = Number(prompt('Enter a number: '));
-let percent = Number(prompt('Enter a %: ')) / 100;
-let percentage = number * percent;
-let sum = number + percentage1512;
-alert(`${percent} of ${number} = ${percentage}`);
-alert(`${number} + ${percentage} = ${sum}`);
+// const sum = (a, b) => a + b;
+
+// function percentCalculate(number, percent) {
+//     let percentage = number * (percent / 100);
+//     return sum(number, percentage);
+
+// }
+
+// console.log(percentCalculate(10, 15));
+
+
+
+// *****************  Array methods (push, lenght, slice, indexOf)  *****************
+
+// let components  = ['motherBoard','CPU','GPU','RAM','hardDrive'];
+// components.push('powerSupply');
+// components.push('cooler');
+// console.log(components);
+// console.log(components.length);
+// // console.log(components.slice(0, 5));
+// // console.log(components.slice(3));
+// console.log(components.indexOf('cooler'));
+
+
+
+// *****************  Objects  *****************
+
+// let introducer = (name, lastName, age, eyeColor ) => {
+//     let person = {
+//         name: name,
+//         lastName: lastName,
+//         age: age,
+//         eyeColor: eyeColor,
+//         yearBirth: function yearBirthCalc() {
+//             return new Date().getFullYear() - Number(age);
+//         }
+//     }
+//     let intro = `Hi, my name is ${person.name} and the color of my eyes is ${person.eyeColor} and I'm born in ${person.yearBirth()}`;
+//     return intro;
+// }
+// console.log(introducer('Sebastian', 'Szewczyk', '20', 'green'));
+
+
+
