9056c112bdea6a09aafe90d303e85e45421249f3
Kimimate <103746985+Kimimate@users.noreply.github.com>
2022-09-13 13:59:02 +0200

    JavasSript calculateBill

--- JavaScript-Lerning/TotalTipCalculator/main.js
+++ JavaScript-Lerning/TotalTipCalculator/main.js
@@ -1,6 +1,19 @@
-let bill = document.getElementById('billTotalInput');
-let tip = document.getElementById('tipInput');
-let minus;
-let plus;
-let total = document.getElementById('perPersonTotal').innerText;
-document.getElementById('perPersonTotal').innerText = 'aa';
+let billTotalInput = document.getElementById('billTotalInput');
+let tipInput = document.getElementById('tipInput');
+let perPersonTotal = document.getElementById('perPersonTotal');
+let numberOfPeople = document.getElementById('numberOfPeople');
+
+let people = Number(numberOfPeople.innerText);
+
+const calculateBill = () => {
+    const bill = Number(billTotalInput.value);
+    const tip = Number(tipInput.value / 100);
+    const total = (bill + (bill * tip)) / people;
+    perPersonTotal.innerText = `$${total.toLocaleString('en-US')}`;
+}
+
+const decreasePeople = () => {}
+
+const increasePeople = () => {
+    
+}
