bf81d5d98640e59c2f2a4d35445bf1f5564b211c
Kimimate <103746985+Kimimate@users.noreply.github.com>
2022-09-18 18:21:29 +0200

    Update html & css

---
+++ JavaScript-Lerning/RockPaperScissorsGame/main.js
@@ -0,0 +1,11 @@
+const randomInt = () => {
+    return Math.floor(Math.random() * 10);
+}
+console.log(randomInt());
+console.log(randomInt());
+console.log(randomInt());
+console.log(randomInt());
+console.log(randomInt());
+console.log(randomInt());
+console.log(randomInt());
+console.log(randomInt());
