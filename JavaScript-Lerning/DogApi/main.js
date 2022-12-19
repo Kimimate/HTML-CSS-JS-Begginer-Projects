188c7d5d70da8b840971fb29bd28ec927f936f5c
Kimimate <103746985+Kimimate@users.noreply.github.com>
2022-12-19 17:30:38 +0100

    .

--- JavaScript-Lerning/DogApi/main.js
+++ JavaScript-Lerning/DogApi/main.js
@@ -1,3 +1,16 @@
-fetch('https://dog.ceo/api/breeds/image/random')
+console.log('hello');
+
+
+
+function rndImgDog() {
+    fetch("https://dog.ceo/api/breeds/image/random") 
     .then(Response => Response.json())
-    .then(json => console.log(json))
+    .then(json => {
+        document.getElementById('dog').src = json.message
+    })
+}
+
+let reset = document.getElementById('reset');
+reset.onclick = () => rndImgDog();
+
+rndImgDog()
